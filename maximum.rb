7e6ed12799c8795ceb0a45a2f6a580ad30f13726
rraciop <ryanracioppo@gmail.com>
2013-08-20 12:58:06 -0400

    initial commit

--- maximum.rb
+++ maximum.rb
@@ -1,5 +1,5 @@
 def maximum(arr)
-	arr.max
+	arr.max      #necessary code to complete rubeque exercise
 end
 
 set=[2,42,22,02]
@@ -8,5 +8,6 @@ set=[-2,0,33,304,2,-2]
 puts maximum(set)
 set=[1]
 puts maximum(set)
+#assert_equal maximum([2, 42, 22, 02]), 42
 #assert_equal maximum([-2, 0, 33, 304, 2 -2]), 304
 #assert_equal maximum([1]), 1
